a0585018e42a2143fa82b999abc8add17d9a6ac9
reijiokito <romeothecong@gmail.com>
2020-09-04 21:41:50 +0700

    ??

--- doctor-frontend/src/redux/package/saga.js
+++ doctor-frontend/src/redux/package/saga.js
@@ -368,7 +368,7 @@ function* watchUpdateAppointmentPackageWorker(action) {
             token
         );
         if (!_.isEmpty(result)) {
-            yield put(getPackageAppointments(action?.packageId))
+            yield put(getPackageAppointments(action?.packageId));
             yield put(getAppointmentsFromTo(action?.doctorId, 
                 moment().format('YYYY-MM-DD'), moment().add(12, 'days').format('YYYY-MM-DD')));
             message.success("Sửa thành công")
